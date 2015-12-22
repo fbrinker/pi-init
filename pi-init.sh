f7f128beb9ddfb2e955654bc8bc5b48d733ad035
Florian Brinker <fbrinker@users.noreply.github.com>
2015-12-22 22:59:03 +0100

    Update pi-init.sh

--- pi-init.sh
+++ pi-init.sh
@@ -38,6 +38,7 @@ cd ~
 
 echo "#"
 echo "# Now, please add the following key to your Github profile, otherwise we can't continue... :)"
+echo "# https://github.com/settings/ssh"
 echo "# ----------"
 cat .ssh/id_rsa.pub
 echo "# ----------"
@@ -77,3 +78,34 @@ interface eth0
     static ip_address=$PI_IP/24
     static routers=${PI_IP_ARRAY[0]}.${PI_IP_ARRAY[1]}.${PI_IP_ARRAY[2]}.1
     static domain_name_servers=${PI_IP_ARRAY[0]}.${PI_IP_ARRAY[1]}.${PI_IP_ARRAY[2]}.1" >> /etc/dhcpcd.conf
+
+#
+# Done
+#
+echo "#"
+echo "# You may want to check the following files for a correct configuration:"
+echo "# /etc/hostname:"
+echo "# ----------"
+sudo cat /etc/hostname
+echo "# ----------"
+echo "#"
+read PI_TMP
+echo "# /etc/hosts:"
+echo "# ----------"
+sudo cat /etc/hosts
+echo "# ----------"
+echo "#"
+read PI_TMP
+echo "# /etc/dhcpcd.conf:"
+echo "# ----------"
+sudo cat /etc/dhcpcd.con
+echo "# ----------"
+echo "#"
+read PI_TMP
+
+echo "#"
+echo "# Now, if everything is correct, please reboot the device."
+echo "# sudo reboot"
+echo "#"
+echo "# Thanks. Bye :)"
+echo "#"
