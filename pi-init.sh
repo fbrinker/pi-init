fba34c1c48063858cc4ccbbe30c22f6cede6fbee
Florian Brinker <fbrinker@users.noreply.github.com>
2015-12-22 22:16:23 +0100

    Create pi-init.sh

---
+++ pi-init.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+BASEIP="192.168.178."
+
+# Input
+read -e -p "The Name of the PI: " -i "pi@" PI_NAME
+echo "Hello. From now on, my name is pi@$PI_NAME"
+echo ""
+echo ""
+read -e -p "My IP: " -i "$BASEIP" PI_IP
+echo "Okay. My IP will be $PI_IP"
+echo ""
+read -e -p "Ready to start the installation? " -i "y" PI_START
+
+if [[ $PI_START != "y" ]]; then
+    exit
+fi
+
+# Run
+### Updates
+sudo apt-get update && apt-get upgrade --yes
+sudo apt-get install vim git
+
+### Generate Key
+cd ~
+ssh-keygen -t rsa -b 4096 -C $PI_NAME
+
+echo "Now, please add the following key to your Github profile, otherwise we can't continue... :)"
+cat .ssh/id_rsa.pub
+read PI_TMP
+
+### Checkout && install my dotfiles
+git clone https://github.com/fbrinker/dotfiles.git
+~/dotfiles/install
+
+### Update ip config
+cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
+
+declare -a PI_IP_ARRAY
+PI_IP_ARRAY=(`echo ${PI_IP//./ }`)
+
+echo "
+#
+# static config
+#
+interface eth0
+    static ip_address=$PI_IP/24
+    static routers=${IPARR[0]}.${IPARR[1]}.${IPARR[2]}.1
+    static domain_name_servers=${IPARR[0]}.${IPARR[1]}.${IPARR[2]}.1"# >> /etc/dhcpcd.conf
